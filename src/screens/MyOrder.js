c838f2b21f3e5232dc2e39d81e0c2cac2302aa35
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-24 17:23:53 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5000/api/myOrderData", {
+    await fetch("https://foodbhandar-3.onrender.com/api/myOrderData", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: "POST",
