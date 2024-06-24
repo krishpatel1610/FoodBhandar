d18c048f54813eb22c82b7f7bf017ec03ef84fdc
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-24 17:23:25 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/loginuser", {
+        const response = await fetch("https://foodbhandar-3.onrender.com/api/loginuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
