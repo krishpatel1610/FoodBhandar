c5e4b51128f9b73e8e5517ee45b30737d76f4170
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-24 17:23:04 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     const [foodItem,setFoodItem] = useState([])
     
     const loadData = async ()=>{
-        let response = await fetch("http://localhost:5000/api/foodData",{
+        let response = await fetch("https://foodbhandar-3.onrender.com/api/foodData",{
             method:"POST",
             headers:{
                 'Content-Type':'application/json'
