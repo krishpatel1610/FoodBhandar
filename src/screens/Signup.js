2c88522de699add280a85be66db094eff50d822a
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-06-24 17:24:18 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -12,7 +12,7 @@ const Signup = () => {
 
     const handleSubmit = async(e)=>{
         e.preventDefault(); // synthetic event read once
-        const response = await fetch("http://localhost:5000/api/createuser",{
+        const response = await fetch("https://foodbhandar-3.onrender.com/api/createuser",{
             method:'POST',
             headers: {
                 'Content-Type':'application/json',
